1a73cb80b776d9697b517b3fab58917fc311fce2
strk-99 <pvsuryateja99@gmail.com>
2026-03-24 15:02:03 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+    base: '/surya-portfolio-website/', // Set this to your repository name for GitHub Pages
     appType: 'mpa', // Explicitly disable SPA fallback
     build: {
         rollupOptions: {
